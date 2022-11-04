cf1bad06d6f89163a05da77dec26f58e847d80f4
Alireza Asadi <alireza.asadi.36@gmail.com>
2022-11-04 15:26:59 +0330

    Initial commit.

---
+++ README.md
@@ -0,0 +1,3 @@
+# swift-localization-backport
+
+A description of this package.
---
+++ Tests/LocalizationBackportTests/swift_localization_backportTests.swift
@@ -0,0 +1,6 @@
+import XCTest
+@testable import LocalizationBackport
+
+final class SwiftLocalizationBackportTests: XCTestCase {
+    
+}
